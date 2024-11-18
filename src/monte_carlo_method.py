b5a293eebcf7159760cd226cf2452e9fc5b34872
ARCHIBALD.CARRION <86129665+archibald-carrion@users.noreply.github.com>
2024-11-18 15:17:26 -0600

    add better internal documentation to the backend function

--- src/monte_carlo_method.py
+++ src/monte_carlo_method.py
@@ -1,12 +1,12 @@
 import random
 
 
-def monte_carlo_pi(n):
+def monte_carlo_pi(points_quantity):
     """
     Estimate the value of pi using the Monte Carlo method.
 
     Parameters:
-    - n (int): The number of random points to generate.
+    - points_quantity (int): The number of random points to generate.
 
     Returns:
     - pi_approximation (float): The estimated value of pi.
@@ -19,17 +19,23 @@ def monte_carlo_pi(n):
     x_inside, y_inside = [], []
     x_outside, y_outside = [], []
     
-    for _ in range(n):
-        x = random.uniform(-1, 1)
-        y = random.uniform(-1, 1)
+    # Generate random points and check if they are inside the circle
+    for _ in range(points_quantity):
+        x = random.uniform(-1, 1) # Generate random x-coordinate from -1 to 1
+        y = random.uniform(-1, 1) # Generate random y-coordinate from -1 to 1
         
-        if x*x + y*y <= 1:
-            inside_circle += 1
+        is_inside = x**2 + y**2 <= 1
+
+        inside_circle += is_inside # Increment if the point is inside the circle
+
+        if is_inside:
             x_inside.append(x)
             y_inside.append(y)
         else:
             x_outside.append(x)
             y_outside.append(y)
+
     
-    pi_approximation = 4 * inside_circle / n
+    # formula to calculate the estimated value of pi
+    pi_approximation = 4 * inside_circle / points_quantity
     return pi_approximation, x_inside, y_inside, x_outside, y_outside
